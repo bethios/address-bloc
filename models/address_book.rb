6ea22993d5e3ab37abc2e2cd9d770b7d3950bbf9
Beth Cummings <bethios@gmail.com>
2017-03-19 19:15:30 -0500

    added ability to import from csv

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,4 +1,5 @@
 require_relative 'entry'
+require "csv"
 
 class AddressBook
   attr_reader :entries
@@ -7,29 +8,29 @@ class AddressBook
     @entries = []
   end
 
-def add_entry(name, phone_number, email)
-    index = 0
-    entries.each do |entry|
-      if name < entry.name
-        break
+  def add_entry(name, phone_number, email)
+      index = 0
+      entries.each do |entry|
+        if name < entry.name
+          break
+        end
+        index+= 1
       end
-      index+= 1
+      entries.insert(index, Entry.new(name, phone_number, email))
     end
-    entries.insert(index, Entry.new(name, phone_number, email))
+
+
+  def remove_entry(name, phone_number, email)
+      entries.delete_if{|entry| entry.name == name }
   end
 
+  def import_from_csv(file_name)
+    csv_text = File.read(file_name)
+    csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+    csv.each do |row|
+       row_hash = row.to_hash
+       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+     end
+  end
 
-def remove_entry(name, phone_number, email)
-    entries.delete_if{|entry| entry.name == name }
-=begin
-    index = 0
-    entries.each do |entry|
-      if name == entry.name
-      entries.delete_at(index)
-        break
-      end
-      index += 1
-    end
-=end
-end
 end
